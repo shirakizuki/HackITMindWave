88c1f1b278e6f9fab8bed4363ddfa6a046923765
Shirazaki <tigoypetercharles@outlook.com>
2025-07-15 09:20:51 +0800

    feat: add initial tab layout and screens for devices, profile, and trends

--- mindwave/app/(root)/(tabs)/_layout.tsx
+++ mindwave/app/(root)/(tabs)/_layout.tsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
 
 const _layout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <Tabs>
+        
+    </Tabs>
   )
 }
 
