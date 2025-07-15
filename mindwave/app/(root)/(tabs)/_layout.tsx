868962f2b0af61e882e1b9c8503d19a023055c6d
Shirazaki <tigoypetercharles@outlook.com>
2025-07-15 09:23:18 +0800

    feat: implement custom tab bar component for improved navigation

--- mindwave/app/(root)/(tabs)/_layout.tsx
+++ mindwave/app/(root)/(tabs)/_layout.tsx
@@ -2,10 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 
+
 const _layout = () => {
   return (
-    <Tabs>
-        
+    <Tabs tabBar={(props) => <TabBar {...props} />}>
+      <Tabs.Screen name="index" options={{ title: 'Home', headerShown: false }} key={1} />
+      <Tabs.Screen name="trends" options={{ title: 'Trends', headerShown: false }} key={2} />
+      <Tabs.Screen name="devices" options={{ title: 'Devices', headerShown: false }} key={3} />
+      <Tabs.Screen name="profile" options={{ title: 'Profile', headerShown: false }} key={4} />
     </Tabs>
   )
 }
