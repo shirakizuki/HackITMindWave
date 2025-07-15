cdff2dbe944643cad90498079998207db445ddb5
Lowell Grey <banayuwelllo@gmail.com>
2025-07-15 09:25:17 +0800

    ggs

---
+++ mindwave/app/(root)/(tabs)/devices.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const devices = () => {
+  return (
+    <View>
+      <Text>devices</Text>
+    </View>
+  )
+}
+
+export default devices
---
+++ mindwave/app/(root)/(tabs)/trends.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const trends = () => {
+  return (
+    <View>
+      <Text>trends</Text>
+    </View>
+  )
+}
+
+export default trends
